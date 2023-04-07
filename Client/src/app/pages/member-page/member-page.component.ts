b89bbc483f1f61a3b14383dc307c5c950affd829
Gia-Sherwin Ly <ly.giasherwin@gmail.com>
2023-04-07 14:55:23 -0400

    page membre avec paginateur

--- Client/src/app/pages/member-page/member-page.component.ts
+++ Client/src/app/pages/member-page/member-page.component.ts
@@ -18,6 +18,20 @@ const DATA : CoopMember [] = [
     mailingAdress: 'johndoe@example.com',
     email: 'johndoe@example.com',
     annualMembership : 50,
+  },
+  {
+    idMember: '007',
+    idBankAccount: 'DDD',
+    memberName: 'John Doe',
+    preferredParking: 'Ghetto',
+    memberPassword: 'password',
+    licenseNo: '0123456',
+    entityType: 'PERSON',
+    birthDate: '2000/02/22',
+    lastAccidentDate: '2020/01/01',
+    mailingAdress: 'Enfer',
+    email: 'johndoe@example.com',
+    annualMembership : 50,
   }
 ]
 
