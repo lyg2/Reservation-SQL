b11991c497009d55865c89ce9ad5cc4e41476363
Gia-Sherwin Ly <ly.giasherwin@gmail.com>
2023-04-07 11:00:51 -0400

    interfaces implementation

---
+++ common/tables/carshare-member.ts
@@ -0,0 +1,5 @@
+import { CoopMember } from "./coop-member";
+
+export interface CarShareMember extends CoopMember {
+
+}
---
+++ common/tables/coop-member.ts
@@ -0,0 +1,10 @@
+export interface CoopMember {
+    idMember: string;
+    memberPassword: string;
+    licenseNo: string;
+    entityType: string;
+    birthDate: string;
+    lastAccidentDate: string;
+    mailingAdress: string;
+    email: string;
+}
