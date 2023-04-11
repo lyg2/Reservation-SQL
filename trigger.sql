a64937ae0d736d858f4e69a2aefe49678325edcc
Gia-Sherwin Ly <ly.giasherwin@gmail.com>
2023-04-10 21:00:51 -0400

    update data

--- trigger.sql
+++ trigger.sql
@@ -96,4 +96,20 @@ CREATE TRIGGER setOdometerStart BEFORE INSERT ON Reservation
 FOR EACH ROW EXECUTE FUNCTION set_odometer_start();
 
 
--- Use of Angular observable to check if end of reservation is met
+CREATE FUNCTION set_membership() RETURNS trigger AS $onCarShareMemberInsert$
+	DECLARE birthDate Date := (SELECT birthDate FROM CoopMember WHERE idMember=NEW.idMember);
+	DECLARE lastAccidentDate Date := (SELECT lastAccidentDate FROM CoopMember WHERE idMember=NEW.idMember);
+	DECLARE age INT := (CURRENT_DATE - birthDate) / 360;
+	DECLARE yearsSinceLastAccident INT := (CURRENT_DATE - lastAccidentDate) / 360;
+	BEGIN
+		IF age > 25 AND (yearsSinceLastAccident IS NULL OR yearsSinceLastAccident >= 1) THEN
+			NEW.annualMembership = 75;
+		ELSE
+			NEW.annualMembership = 100;
+		END IF;
+		RETURN NEW;
+	END; $onCarShareMemberInsert$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER setMembership BEFORE INSERT ON CarShareMember
+FOR EACH ROW EXECUTE FUNCTION set_membership();
